c7ba35ca5514e7b8965ae12b5feaba85d8d57890
Murtuza <68743212+murtuzaalisurti@users.noreply.github.com>
2021-05-20 22:41:45 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".img_contain").addEventListener("mouseover", () => {
-    document.querySelector(".container").classList.add("spotlight");
-})
-document.querySelector(".img_contain").addEventListener("mouseleave", () => {
-    document.querySelector(".container").classList.remove("spotlight");
-})
